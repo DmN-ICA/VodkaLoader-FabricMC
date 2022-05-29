ff0b90b5c5b89a5088af9404296885c80b8e190c
DomamaN202 <51743763+Domaman202@users.noreply.github.com>
2022-05-29 14:06:41 +0700

    xD

---
+++ src/main/java/DmN/ICA/vodka/impl/VodkaLoader.java
@@ -0,0 +1,10 @@
+package DmN.ICA.vodka.impl;
+
+import net.fabricmc.api.ModInitializer;
+
+public class VodkaLoader implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
