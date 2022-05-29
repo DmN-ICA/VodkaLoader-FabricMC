918eeb56402e763e0c0d9f64dfd611b5fb1fe7c1
DomamaN202 <51743763+Domaman202@users.noreply.github.com>
2022-05-29 14:19:03 +0700

    hm...

--- src/main/java/DmN/ICA/vodka/impl/VodkaLoader.java
+++ src/main/java/DmN/ICA/vodka/impl/VodkaLoader.java
@@ -1,10 +1,22 @@
 package DmN.ICA.vodka.impl;
 
+import DmN.ICA.vodka.api.MinecraftVersion;
 import net.fabricmc.api.ModInitializer;
+import org.jetbrains.annotations.NotNull;
 
-public class VodkaLoader implements ModInitializer {
+public class VodkaLoader extends DmN.ICA.vodka.VodkaLoader implements ModInitializer {
     @Override
     public void onInitialize() {
 
+    }
+
+    @Override
+    public @NotNull MinecraftVersion getMCVersion() {
+        return new MinecraftVersion.;
+    }
+
+    @Override
+    public @NotNull LoaderType getLoaderType() {
+        return LoaderType.Fabric;
     }
 }
