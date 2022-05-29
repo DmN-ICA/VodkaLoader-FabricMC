2ad7995082d71acb6201cd6307d01d99901b324d
DomamaN202 <51743763+Domaman202@users.noreply.github.com>
2022-05-29 16:56:27 +0700

    nice

--- src/main/java/DmN/ICA/vodka/impl/VodkaLoader.java
+++ src/main/java/DmN/ICA/vodka/impl/VodkaLoader.java
@@ -1,13 +1,26 @@
 package DmN.ICA.vodka.impl;
 
 import DmN.ICA.vodka.api.MinecraftVersion;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import org.jetbrains.annotations.NotNull;
 
-public class VodkaLoader extends DmN.ICA.vodka.VodkaLoader implements ModInitializer {
+public class VodkaLoader extends DmN.ICA.vodka.VodkaLoader implements ModInitializer, ClientModInitializer, DedicatedServerModInitializer {
     @Override
     public void onInitialize() {
+        super.commonInit();
+    }
 
+    @Override
+    public void onInitializeClient() {
+        super.clientInit();
+    }
+
+    @Override
+    public void onInitializeServer() {
+        super.serverInit();
     }
 
     @Override
@@ -18,5 +31,10 @@ public class VodkaLoader extends DmN.ICA.vodka.VodkaLoader implements ModInitial
     @Override
     public @NotNull LoaderType getLoaderType() {
         return LoaderType.Fabric;
+    }
+
+    @Override
+    public @NotNull EnvType getEnvironment() {
+        return EnvType.valueOf(FabricLoader.getInstance().getEnvironmentType().name());
     }
 }
